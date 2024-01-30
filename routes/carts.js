61782f7ea117615d012d2e88c3ed35ccfc0b9a45
Lay <lay.merabti@gmail.com>
2024-01-30 16:50:35 +0100

    ok

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-//Get trips by departure/arrival/date
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const Cart = require('../models/carts');
+
+//Post with find cart
+router.post('/', (req, res) => {
+  const {departure, arrival, date, price} = req.body
+  const newCart = new Cart({
+    departure: departure,
+    arrival: arrival,
+    date: date,
+    price: price,
+    isValable: true
+  });
+	newCart.save().then(data => {
+						res.json({cart: data });
+					});
+})
+
+router.get('/', (req, res) => {
+	Cart.find().then(data => {
+						res.json({cart: data });
+					});
+})
+
+router.delete("/:id", (req, res) => {
+  const {id} = req.params;
+  Cart.updateOne({ _id: id }, {isValable: false}).then(data => {
+  res.json({cart: data });
+  })
 });
 
 
