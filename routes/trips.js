d762725cf4d711024cb8bf3ff88a3b6c1c2e7e4c
Your name <Your email>
2024-01-30 16:36:28 +0100

    router.get trips by departure/arrival/date

--- routes/trips.js
+++ routes/trips.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
-//Get trips by departure/arrival/date
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const Trip = require("../models/trips");
+
+//Get trips by departure/arrival
+
+router.get('/',  (req, res) => {
+  console.log(new Date(moment(req.body.date).startOf('day')))
+ 
+  Trip.find({departure:{ $regex: new RegExp(req.body.departure, "i")}, arrival: { $regex: new RegExp(req.body.arrival, "i")}, date: { $gte: new Date(moment(req.body.date).startOf('day').utc(0)), $lte: new Date(moment(req.body.date).endOf('day').utc(0)) } 
+  
+}).then(data => {
+  if (data.length>0) {
+    res.json({result: true, trips: data});
+  } else {
+    res.json({ result: false, error: "Trip not found"});
+  }
+})
 });
+
 
 
 
