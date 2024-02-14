6afc54014948cc317429598593bd24697dfd49e1
NicolasNgu23 <nicolasnguyen0023@hotmail.com>
2024-02-14 16:25:08 +0100

    final

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,6 @@
-
-
   const mongoose = require('mongoose');
 
-  const connectionString = process.env.CONNECTION_STRING;
+  const connectionString = 'mongodb+srv://nicolasnguyen0023:iffy@cluster0.zat8bpq.mongodb.net/traindb';
 
   mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
     .then(() => console.log('Database connected'))
